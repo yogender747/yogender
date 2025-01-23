25847411c6b7d1dcb2eaf45d115c7a0c6076e436
Yogender <129525898+yogender747@users.noreply.github.com>
2025-01-23 19:45:11 +0530

    Add files via upload

---
+++ rishi.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <time.h>
+
+#define BUFFER_SIZE 9000
+
+// Global variables
+char *ip;
+int port;
+int duration;
+
+// Function to send UDP traffic
+void *send_udp_traffic(void *arg) {
+    int sock;
+    struct sockaddr_in server_addr;
+    char buffer[BUFFER_SIZE];
+    int sent_bytes;
+
+    // Create a socket
+    if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("Socket creation failed");
+        pthread_exit(NULL);
+    }
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    if (inet_pton(AF_INET, ip, &server_addr.sin_addr) <= 0) {
+        perror("Invalid address/ Address not supported");
+        close(sock);
+        pthread_exit(NULL);
+    }
+
+    snprintf(buffer, sizeof(buffer), "UDP traffic test");
+
+    time_t start_time = time(NULL);
+    time_t end_time = start_time + duration;
+
+    while (time(NULL) < end_time) {
+        sent_bytes = sendto(sock, buffer, strlen(buffer), 0,
+                            (struct sockaddr *)&server_addr, sizeof(server_addr));
+        if (sent_bytes < 0) {
+            perror("Send failed");
+            close(sock);
+            pthread_exit(NULL);
+        }
+    }
+
+    close(sock);
+    pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        fprintf(stderr, "Usage: %s <IP> <PORT> <DURATION>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    ip = argv[1];
+    port = atoi(argv[2]);
+    duration = atoi(argv[3]);
+
+    const int threads = 60;
+    pthread_t tid[threads];
+
+    for (int i = 0; i < threads; i++) {
+        if (pthread_create(&tid[i], NULL, send_udp_traffic, NULL) != 0) {
+            perror("Thread creation failed");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    for (int i = 0; i < threads; i++) {
+        pthread_join(tid[i], NULL);
+    }
+
+    return 0;
+}
